48e1a3779ec6f96f173f0a915c606f5a3745354a
Robert Sandor <robert.sandor@rt-rk.com>
2018-06-07 15:53:41 +0200

    Initial commit; Splash screen created

---
+++ app/src/main/java/com/example/rtrk/restfulclient/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.example.rtrk.restfulclient;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Intent mMainIntent = new Intent(this, MainActivity.class);
+        startActivity(mMainIntent);
+        finish();
+    }
+}
